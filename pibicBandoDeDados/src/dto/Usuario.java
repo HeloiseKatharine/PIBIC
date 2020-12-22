87fc6e5769274ad4a954925d81662a411d695d87
Heloise Katharine <katharineheloise@gmail.com>
2020-12-22 13:07:41 -0300

    Usuario

---
+++ pibicBandoDeDados/src/dto/Usuario.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class Usuario {
+
+    private int id;
+    private String usuario, senha;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
