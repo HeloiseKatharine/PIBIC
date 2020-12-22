a170cfbde7e53b4bc1769548920abe7b5d1b6bed
Heloise Katharine <katharineheloise@gmail.com>
2020-12-22 13:07:33 -0300

    Paciente

---
+++ pibicBandoDeDados/src/dto/Paciente.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class Paciente {
+
+    private String cpf, nome;
+    private int id_paciente;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId_paciente() {
+        return id_paciente;
+    }
+
+    public void setId_paciente(int id_paciente) {
+        this.id_paciente = id_paciente;
+    }
+
+}
