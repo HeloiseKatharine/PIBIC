3a93f1adb9f64bd582c23495aab598176f45d1a9
Heloise Katharine <katharineheloise@gmail.com>
2020-12-29 00:06:17 -0300

    UsuarioDAO

--- pibicBandoDeDados/src/dao/UsuarioDAO.java
+++ pibicBandoDeDados/src/dao/UsuarioDAO.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package dao;
 
 import dto.Usuario;
@@ -6,10 +11,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-
+/**
+ *
+ * @author heloh
+ */
 public class UsuarioDAO {
-
-    Connection conn;
+     Connection conn;
 
     public ResultSet autenticacaoUsuario(Usuario objUsuario) {
 
