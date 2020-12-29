0783f13cfa31b4205a2476b4dd8d1c4d3c9de5d9
Heloise Katharine <katharineheloise@gmail.com>
2020-12-29 00:06:27 -0300

    PacienteDAO

--- pibicBandoDeDados/src/dao/PacienteDAO.java
+++ pibicBandoDeDados/src/dao/PacienteDAO.java
@@ -14,13 +14,16 @@ public class PacienteDAO {
 
     Connection conn;
     PreparedStatement pstm;
-    ResultSet rs;
-    ArrayList<Paciente> lista = new ArrayList<>();
+    PreparedStatement pstm2;
+    ResultSet rs2;
 
-    public void cadastrarPaciente(Paciente objPaciente, Exame objExame) {
+    ResultSet rs;
+    ArrayList<Exame> lista = new ArrayList<>();
+
+    public void cadastrarPaciente(Paciente objPaciente, Exame objExame, boolean img) {
 
         String sql = "insert into paciente (cpf, nome) values (?,?);";
-        String sql2 = " insert into exame (caracteristicatecido,classeanormalidade,gravidadeanormalidade, x, y, raio, paciente_cpf, paciente_idpaciente) values(?, ?, ?, ?, ?, ?, ?, LAST_INSERT_ID()); ";
+        String sql2 = " insert into exame (caracteristicatecido,classeanormalidade,gravidadeanormalidade, x, y, raio, nomeimagem, paciente_cpf, paciente_idpaciente) values(?, ?, ?, ?, ?, ?, ?, ?, LAST_INSERT_ID()); ";
         conn = new ConexaoDAO().conexaoBD();
 
         try {
@@ -36,37 +39,41 @@ public class PacienteDAO {
             JOptionPane.showMessageDialog(null, "PacienteDAO " + e);
         }
 
-        try {
-            pstm = conn.prepareStatement(sql2);
-            pstm.setString(1, objExame.getCaracteristicatecido());
-            pstm.setString(2, objExame.getClasseanormalidade());
-            pstm.setString(3, objExame.getGravidadeanormalidade());
+        if (img == true) {
 
-            if (objExame.getX() == null) {
-                pstm.setNull(4, Types.INTEGER);
-            } else {
-                pstm.setInt(4, objExame.getX());
+            try {
+                pstm = conn.prepareStatement(sql2);
+                pstm.setString(1, objExame.getCaracteristicatecido());
+                pstm.setString(2, objExame.getClasseanormalidade());
+                pstm.setString(3, objExame.getGravidadeanormalidade());
+
+                if (objExame.getX() == null) {
+                    pstm.setNull(4, Types.INTEGER);
+                } else {
+                    pstm.setInt(4, objExame.getX());
+                }
+
+                if (objExame.getY() == null) {
+                    pstm.setNull(5, Types.INTEGER);
+                } else {
+                    pstm.setInt(5, objExame.getY());
+                }
+
+                if (objExame.getRaio() == null) {
+                    pstm.setNull(6, Types.INTEGER);
+                } else {
+                    pstm.setInt(6, objExame.getRaio());
+                }
+
+                pstm.setString(7, objExame.getNomeimagem());
+                pstm.setString(8, objPaciente.getCpf());
+
+                pstm.execute();
+                pstm.close();
+
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "PacienteDAO " + e);
             }
-
-            if (objExame.getY() == null) {
-                pstm.setNull(5, Types.INTEGER);
-            } else {
-                pstm.setInt(5, objExame.getY());
-            }
-
-            if (objExame.getRaio()== null) {
-                pstm.setNull(6, Types.INTEGER);
-            } else {
-                pstm.setInt(6, objExame.getRaio());
-            }
-
-            pstm.setString(7, objPaciente.getCpf());
-
-            pstm.execute();
-            pstm.close();
-
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "PacienteDAO " + e);
         }
     }
 
@@ -76,7 +83,7 @@ public class PacienteDAO {
 
         try {
 
-            String sql = "select cpf, nome from paciente where cpf = ? and nome = ? ";
+            String sql = "select cpf, nome from paciente where cpf = (?) and nome = (?); ";
             pstm = conn.prepareStatement(sql);
 
             pstm.setString(1, objPaciente.getCpf());
@@ -91,7 +98,7 @@ public class PacienteDAO {
         }
     }
 
-    public boolean mostrarPaciente(Paciente objPaciente) {
+    public boolean mostrarPaciente(Paciente objPaciente, Exame objExame) {
 
         conn = new ConexaoDAO().conexaoBD();
 
@@ -106,9 +113,36 @@ public class PacienteDAO {
 
             //pesquisando no banco
             if (rs.next()) {
-
                 objPaciente.setCpf(rs.getString("cpf"));
                 objPaciente.setNome(rs.getString("nome"));
+                objPaciente.setId_paciente(rs.getInt("idpaciente"));
+
+                try {
+
+                    String sql2 = "select * from exame where paciente_cpf = ?;";
+                    pstm2 = conn.prepareStatement(sql2);
+
+                    pstm2.setString(1, objPaciente.getCpf());
+
+                    rs2 = pstm2.executeQuery();
+
+                    //pesquisando no banco
+                    if (rs2.next()) {
+
+                        objExame.setCaracteristicatecido(rs2.getString("caracteristicatecido"));
+                        objExame.setClasseanormalidade(rs2.getString("classeanormalidade"));
+                        objExame.setGravidadeanormalidade(rs2.getString("gravidadeanormalidade"));
+                        objExame.setX(rs2.getInt("x"));
+                        objExame.setY(rs2.getInt("y"));
+                        objExame.setRaio(rs2.getInt("raio"));
+
+                    }
+                    return true;
+
+                } catch (SQLException e) {
+                    JOptionPane.showMessageDialog(null, "PacienteDAO: " + e);
+                    return false;
+                }
             }
 
             return true;
@@ -119,4 +153,57 @@ public class PacienteDAO {
         }
     }
 
+    public ArrayList<Exame> listarExames(Paciente objPaciente) {
+
+        conn = new ConexaoDAO().conexaoBD();
+
+        String sql = "select * from exame where paciente_cpf = ?;";
+
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objPaciente.getCpf());
+
+            rs = pstm.executeQuery();
+
+            while (rs.next()) {
+                Exame objExame = new Exame();
+                objExame.setCaracteristicatecido(rs.getString("caracteristicatecido"));
+                objExame.setClasseanormalidade(rs.getString("classeanormalidade"));
+                objExame.setGravidadeanormalidade(rs.getString("gravidadeanormalidade"));
+                objExame.setX(rs.getInt("x"));
+                objExame.setY(rs.getInt("y"));
+                objExame.setRaio(rs.getInt("raio"));
+                objExame.setNomeimagem(rs.getString("nomeimagem"));
+
+                lista.add(objExame);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "CinexaoDAO: " + e);
+            return null;
+        }
+        return lista;
+    }
+
+    public void EditarPaciente(Paciente objPaciente) {
+
+        String sql = "update paciente set cpf = ?, nome = ? where idpaciente = ?;";
+
+        conn = new ConexaoDAO().conexaoBD();
+
+        try {
+            pstm = conn.prepareStatement(sql);
+            
+            pstm.setString(1, objPaciente.getCpf());
+            pstm.setString(2, objPaciente.getNome());
+            pstm.setInt(3, objPaciente.getId_paciente());
+            
+            pstm.execute();
+            pstm.close();
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "PacienteDAO " + e);
+        }
+    }
 }
+
+
